a84c14a2545e39164865a01005c03d79569886ac
team_gitee <344330607@qq.com>
2017-12-28 17:06:15 +0800

    测试

--- app/src/main/java/com/example/team/hencoder2/MainActivity.java
+++ app/src/main/java/com/example/team/hencoder2/MainActivity.java
@@ -11,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.e("===","心塞");
+        Log.e("===","心塞");
+        Log.e("===","心塞");
+        Log.e("===","心塞");
+        Log.e("===","心塞");
+        Log.e("===","心塞");
+
+
     }
 }
